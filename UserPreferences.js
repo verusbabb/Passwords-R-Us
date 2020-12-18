b67292e31b3cb9f29d66a319337a7fea34dbf94f
Steve Babb <steve.babb@outlook.com>
2020-12-18 13:27:55 -0600

    fixed an open function syntax error.

--- UserPreferences.js
+++ UserPreferences.js
@@ -1,10 +1,5 @@
 
-        //Create arrays to use in password generator
-        var lcLetters = "abcdefghijklmnopqrstuvwxyz";
-        var lcLettersArray = lcLetters.split(""); //takes one long string value and separates them into individual values
-        var ucLettersArray = lcLetters.toUpperCase().split(""); //quick way to leverage ucLettersArray to create all CAPs array
-        var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        var spCharArray = ["!", "#", "$", "%", "&", "(", "*", "+", "-", "/", ":", ";", "<", ">", "?", "@", "[", "^", "_", "{", "}", "~"]; //eliminated hard-to-type characters
+        
 
         //Declare variables to hold user preference values
         var useCaps = false;
--- script.js
+++ script.js
@@ -4,11 +4,21 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); {
+    
+    //Create arrays to use in password generator
+    var lcLetters = "abcdefghijklmnopqrstuvwxyz";
+    var lcLettersArray = lcLetters.split(""); //takes one long string value and separates them into individual values
+    var ucLettersArray = lcLetters.toUpperCase().split(""); //quick way to leverage ucLettersArray to create all CAPs array
+    var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var spCharArray = ["!", "#", "$", "%", "&", "(", "*", "+", "-", "/", ":", ";", "<", ">", "?", "@", "[", "^", "_", "{", "}", "~"]; 
+
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
