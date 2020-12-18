80240fb89b83cdce77e44fb6629bcb244a0d5d7a
Steve Babb <steve.babb@outlook.com>
2020-12-18 13:14:47 -0600

    created UserPreference.js and now reference it in index.html

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword(); {
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
 
-// }
+}
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
