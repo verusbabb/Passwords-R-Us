449fef1c73d4ff250f95f6e9b65af69f199264e5
Steve Babb <steve.babb@outlook.com>
2020-12-20 22:52:20 -0600

    updated README, index and css edits for styling, commenting throughout

---
+++ README.md
@@ -0,0 +1,62 @@
+# Passwords-R-Us
+Here at Passwords-R-Us, we specialize in creating highly secure passwords that make it difficult for even the best of hackers to steal.  
+
+## "One-in-millon chance of someone guessing your password?"
+Not even close.  If you choose the maximum strength password we can generate, our random password generator technology makes the chances literally 1 in 1,467,574,951,244,021,123,655,345,106,016,837,380,110,615,560,179,184,677,679,415!!!  
+
+A recently published study at www.fakescience.com proved that hackers will actually lose their mind before they crack the passwords we generate. 
+
+So sleep well because Passwords-R-Us is protecting you valuable data.
+
+(Don't just take our word for it.  Reviews are pouring in, as noted on the Passwords-R-Us homepage.)
+
+## Assignment (in all seriousness)
+
+Create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+
+The password can include special characters. 
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Special Efforts
+- ran console.logs on core variables/arrays at several places during debugging to ensure values were setting properly
+- Custom stylized the finished product by modifying html and css.
+- Developed an entirely new brand of password generators and had the Chief Tech Editor at REALLY FAKE NEWS review the product; added quote to website to give it true credibility.
+
+## Finished Product
+
+The following images shows the web application working its magic:
+
+![password generator demo](./Assets/03-javascript-homework-demo.png)
+
+Here is a link to try it for yourself.  We at Passwords"R"Us stongly suggest that you bookmark this application and rely on it for generating passwords to keep your data safe from nasty intruders (tell your parents to as well):
+
+
+----
+Contact:  Steve Babb @ steve.babb@outlook.com
--- script.js
+++ script.js
@@ -5,33 +5,24 @@ var ucLettersArray = lcLetters.toUpperCase().split(""); //leveraging lcletters t
 var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var spCharArray = ["!", "#", "$", "%", "&", "(", "*", "+", "-", "/", ":", ";", "<", ">"];
 
-//Declare variables to hold user preference values
-// var useCaps =undefined;
-// var useLowerCase = undefined;
-// var useNums = undefined;
-// var useSpChars = undefined;
+//Delcare variables to store parts of password develop
 var yourPW = [];
 var password = [];
 
-//Declare variable to hold all relavant characters that user wants to include
-//var includedChars = [];
-
-
-//target the Generate Password button
+//Target the Generate Password button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
 
   //for loop gathering user preferences and creating validation response values
+  //this loop also ensures that the user selects at least one character type
   var i = 0;
 
   let useCaps = false;
   let useLowerCase = false;
   let useNums = false;
   let useSpChars = false;
-
-  //console.log(useCaps, useLowerCase, useNums, useSpChars);
 
   while (i < 1) {
     alert("IMPORTANT: You must select AT LEAST ONE OF Caps, lowercase, numbers, or special characters");
@@ -42,7 +33,7 @@ function writePassword() {
       i++;
       var includeCaps = "CAPS";
     }
-        else { includeCaps = "No CAPS" };
+    else { includeCaps = "No CAPS" };
 
     useLowerCase = confirm("Should the random generator consider including LOWERCASE?");
 
@@ -50,55 +41,57 @@ function writePassword() {
       i++;
       var includeLowerCase = "LOWERCASE letters";
     }
-        else { includeLowerCase = "No LOWERCASE letters" };
+    else { includeLowerCase = "No LOWERCASE letters" };
 
     useNums = confirm("Should the random generator consider including NUMBERS?");
     if (useNums === true) {
       i++;
       var includeNums = "NUMBERS";
     }
-        else { includeNums = "No NUMBERS" };
+    else { includeNums = "No NUMBERS" };
 
     useSpChars = confirm("Should the random generator consider including SPECIAL CHARACTERS?");
     if (useSpChars === true) {
       i++;
       var includeSpChars = "SPECIAL CHARACTERS";
     }
-        else { includeSpChars = "No SPECIAL CHARACTERS" };
+    else { includeSpChars = "No SPECIAL CHARACTERS" };
   }
-  console.log(useCaps, useLowerCase, useNums, useSpChars);
 
-  // Using while loop to gather desired characters; ensure between 8-129
+  // Prompt for number of characters; ensure result is an integer value
   var howMany = parseInt(prompt("Passwords are be between 8 and 128 characters long. Input desired password length."));
 
+  //while loop ensuring number of characters is in range
   while (isNaN(howMany) || howMany < 8 || howMany > 128) {
     var howMany = prompt("Whoops, please enter a desired password length (type in a number between 8-128");
   }
 
   var numChars = parseInt(howMany);
-  var includedChars=[];
+
   //final validation alert of user preferences
   alert("You indicated the resulting password may include :" + "\n" + "\n" + "==> " + includeCaps + "\n" + "==> " + includeLowerCase + "\n" + "==> " + includeNums + "\n" + "==> " + includeSpChars + "\n" + "==> " + "And a total of " + numChars + " characters");
+
+  //creating/setting an array as empty prior to adding user-preferred character types to the array
+  //ensuring the array is empty in the event the writePassword function 
+  //is called more than once in a session
+  var includedChars = [];
 
   //assembling a single array of all user preferred character groups
   if (useCaps === true) {
     includedChars = ucLettersArray;
   }
-  console.log(includedChars);
+  
   if (useLowerCase === true) {
     includedChars = includedChars.concat(lcLettersArray);
   }
-  console.log(includedChars);
 
   if (useNums === true) {
     includedChars = includedChars.concat(numArray);
   }
-  console.log(includedChars);
 
   if (useSpChars === true) {
     includedChars = includedChars.concat(spCharArray);
   }
-  console.log(includedChars);
 
   //running for loop to randomly generate string array with password characters
   for (var j = 0; j < numChars; j++) {
@@ -106,15 +99,14 @@ function writePassword() {
     yourPW.push(pwItem);
   }
 
-  //console.log(yourPW);
-
   password = yourPW.join("");
-
 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  // emptying arrays so that password results start from scratch if
+  //user decides to call the writePassword fuction more than once in a session
   password = [];
   yourPW = [];
 
